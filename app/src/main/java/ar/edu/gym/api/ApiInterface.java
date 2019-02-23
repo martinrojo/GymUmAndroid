358ebbeae2b4b312c36a6acee824e5bf3fd9f97c
bruseghini92 <bruseghini92.ab@gmail.com>
2019-02-22 21:21:21 -0300

    Agregados endpoints createMovement y updateMovement

--- app/src/main/java/ar/edu/gym/api/ApiInterface.java
+++ app/src/main/java/ar/edu/gym/api/ApiInterface.java
@@ -2,6 +2,7 @@ package ar.edu.gym.api;
 
 import java.util.List;
 
+import ar.edu.gym.model.Movimiento;
 import ar.edu.gym.model.Persona;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -22,18 +23,33 @@ public interface ApiInterface {
 
     //(Te trae un usuario unico para cuando quieras editarlo te complete automaticamente los campos del usuario) necesitas mandarle el id
     @Headers(key)
-    @GET("gym/api/personas/{id}") //revisar las url a las q le pegas la httprequest
+    @GET("gym/api/personas/{id}")
+    //revisar las url a las q le pegas la httprequest
     Call<Persona> getUserForId(@Path("id") String id);
 
     //Esta la utilizas para crear un usuario, neecesitas mandarle el modelo completo
     @Headers(key)
-    @POST("gym/api/personas/") //revisar las url a las q le pegas la httprequest
-    Call<Boolean> createUser(@Body Persona persona);
+    @POST("gym/api/personas/")
+    //revisar las url a las q le pegas la httprequest
+    Call<Boolean> createUser(@Body Persona usuarios);
 
     //actualizacion de usuario, mandar modelo con el id
     @Headers(key)
-    @PUT("gym/api/personas/{id}") //revisar las url a las q le pegas la httprequest
-    Call<String> updateUser(@Body Persona persona);
+    @PUT("gym/api/personas/{id}")
+    //revisar las url a las q le pegas la httprequest
+    Call<String> updateUser(@Body Persona usuarios);
+
+    //Esta la utilizas para crear un usuario, neecesitas mandarle el modelo completo
+    @Headers(key)
+    @POST("gym/api/movimientos/")
+    //revisar las url a las q le pegas la httprequest
+    Call<Boolean> createMovement(@Body Movimiento movimiento);
+
+    //actualizacion de usuario, mandar modelo con el id
+    @Headers(key)
+    @PUT("gym/api/movimientos/")
+    //revisar las url a las q le pegas la httprequest
+    Call<String> updateMovement(@Body Movimiento movimiento);
 
 }
 
---
+++ app/src/main/java/ar/edu/gym/model/Movimiento.java
@@ -0,0 +1,63 @@
+package ar.edu.gym.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
+public class Movimiento {
+
+    @SerializedName("id")
+    private Long id;
+
+    @SerializedName("fechaEntrada")
+    private Date fechaEntrada;
+
+    @SerializedName("fechaSalida")
+    private Date fechaSalida;
+
+    @SerializedName("persona")
+    private Persona persona;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    @Override
+    public String toString() {
+        return "Movimiento{" +
+                "id=" + id +
+                ", fechaEntrada=" + fechaEntrada +
+                ", fechaSalida=" + fechaSalida +
+                ", persona=" + persona +
+                '}';
+    }
+
+}
